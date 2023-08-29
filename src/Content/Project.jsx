ec3e067c205aacee650684efa4696878552a636f
Sandip Shrestha <shresthasandip1423.com>
2023-08-29 17:02:39 +0545

    new project added

--- src/Content/Project.jsx
+++ src/Content/Project.jsx
@@ -2,34 +2,45 @@
 // import Profile from "../assets/profile.jpg";
 import { Link } from "react-scroll";
 import web from "../assets/web.png";
+import green from "../assets/green.png";
 import "../style.scss";
 
 function Project() {
   return (
     <>
-      <div className="project">
+      <div className="project row">
         <h1>Projects</h1>
         <Link to="home" className="top mx-2 btn">
           Back To Top
         </Link>
-        <a
-          href="https://shresthasandip.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="card m-5">
+        <div className="card col-lg-6 m-5">
+          <a
+            href="https://shresthasandip.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <div className="card-image">
               <img src={web} alt="" />
             </div>
             <div className="card-title py-3">
               <h2>Web Commerce</h2>
             </div>
-          </div>
-        </a>
-        {/* <div className="card">
-          <div className="card-image"></div>
-          <div className="card-title"></div>
-        </div> */}
+          </a>
+        </div>
+        <div className="card col-lg-6 m-5">
+          <a
+            href="https://gogreen-ngo.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="card-image">
+              <img src={green} alt="" />
+            </div>
+            <div className="card-title py-3">
+              <h2>NGO</h2>
+            </div>
+          </a>
+        </div>
       </div>
     </>
   );
